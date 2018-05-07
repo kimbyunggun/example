7c12f567130593367531230b37c064e9ddf3563b
byunggun <byung.gun.k93@gmail.com>
2018-05-07 13:50:06 +0900

    load_data

--- dataset/open_10cifar.py
+++ dataset/open_10cifar.py
@@ -1,8 +1,19 @@
+import os.path
+import pickle
+import os
+import numpy as np
+
+dataset_dir = os.path.dirname(os.path.abspath(__file__)) #현재 파일의 path의 dirctory_path를 알려준다.
+
+
+print(dataset_dir)
 
 def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
+    file_path = os.path.join(dataset_dir, file) #불러오려고하는 파일의 path
+    print('loading file :',file_path)
+    with open(file_path, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
-    return dict
 
-data = unpickle(C:/User/김동현/workspace/project_1/dataset/cifar-10-python/data_batch_1)
+    return
+
+data = unpickle(file = 'data_batch_1')
