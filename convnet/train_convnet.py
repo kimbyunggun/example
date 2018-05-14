d83d5def29a770660a2981a23c6c64e97b7e2c7e
byunggun <byung.gun.k93@gmail.com>
2018-05-14 09:28:39 +0900

    sgd

--- convnet/train_convnet.py
+++ convnet/train_convnet.py
@@ -22,7 +22,7 @@ network = SimpleConvNet(input_dim=(3,32,32),
 
 trainer = Trainer(network, x_train, t_train, x_test, t_test,
                   epochs=max_epochs, mini_batch_size=100,
-                  optimizer='Adam', optimizer_param={'lr': 0.001},
+                  optimizer='sgd', optimizer_param={'lr': 0.1},
                   evaluate_sample_num_per_epoch=1000)
 trainer.train()
 
