7f3d0ef8cb1731102fcafd0b5e453419ea600fc6
byunggun <byung.gun.k93@gmail.com>
2018-05-17 14:34:04 +0900

    validation_data, momentum(lr0.1)

---
+++ validation_data.py
@@ -0,0 +1,19 @@
+import sys, os
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset.open_10cifar import load_cifar
+from common.multi_layer_net import MultiLayerNet
+from common.util import shuffle_dataset
+from common.trainer import Trainer
+
+(x_train, t_train), (x_test, t_test) = load_cifar(normalize=True)
+
+# 20%를 검증 데이터로 분할
+validation_rate = 0.20
+validation_num = x_train.shape[0] * validation_rate
+x_train, t_train = shuffle_dataset(x_train, t_train)
+x_val = x_train[:validation_num]
+t_val = t_train[:validation_num]
+x_train = x_train[validation_num:]
+t_train = t_train[validation_num:]
